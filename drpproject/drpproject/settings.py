2d4b7586b9323a1f5dd40bf1d97a11e191818432
dys21 <90960756+dys21@users.noreply.github.com>
2023-05-31 17:42:37 +0100

    dys21:<CI/CD fix?> fix STATIC_ROOT appending

--- drpproject/drpproject/settings.py
+++ drpproject/drpproject/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR + "/staticfiles"
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
