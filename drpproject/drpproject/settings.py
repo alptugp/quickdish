1505d931310d56e3a205f34ac907af16998cf746
dys21 <90960756+dys21@users.noreply.github.com>
2023-06-01 21:38:13 +0100

    dys21:<DB> setup Heroku Postgres

--- drpproject/drpproject/settings.py
+++ drpproject/drpproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,13 +78,8 @@ WSGI_APPLICATION = 'drpproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# TODO: Change to postgres
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_health_checks=True, conn_max_age=600, ssl_require=True)
 }
 
 
