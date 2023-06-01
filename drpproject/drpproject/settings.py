88e3a36b0cc66a91c16aecf3e821abbd5774e152
dys21 <90960756+dys21@users.noreply.github.com>
2023-06-01 21:48:54 +0100

    dys21:<DB fix?> conditional DB selection depending on environment

--- drpproject/drpproject/settings.py
+++ drpproject/drpproject/settings.py
@@ -78,9 +78,18 @@ WSGI_APPLICATION = 'drpproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config(conn_health_checks=True, conn_max_age=600, ssl_require=True)
-}
+inHerokuEnvironment = 'DATABASE_URL' in os.environ
+if inHerokuEnvironment:
+    # Heroku Postgres
+    DATABASES = {
+        'default': dj_database_url.config(conn_health_checks=True, conn_max_age=600, ssl_require=True)
+    }
+else:
+    # Local version of SQLite
+    DATABASES = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 
 # Password validation
