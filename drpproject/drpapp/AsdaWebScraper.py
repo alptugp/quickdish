1b3cd020ba213204020454714aa02560f402475d
alptugp <alptugati@gmail.com>
2023-06-02 13:26:59 +0100

    aap221: <> Make api calls for general ingredients faster

--- drpproject/drpapp/AsdaWebScraper.py
+++ drpproject/drpapp/AsdaWebScraper.py
@@ -4,6 +4,16 @@ from apify_client import ApifyClient
 client = ApifyClient("apify_api_dqzb5ZnhW0iNL7DRoC43yl86ucC9KE3fq6fu")
 
 def getMostRelevantItemAsda(query):
+    if query == "milk": 
+        return {'price': {'price_info': {'price': "£0.90"}}, 'item': {'sku_id':  "37518"}} 
+    if query == "eggs": 
+        return {'price': {'price_info': {'price': "£2.50"}}, 'item': {'sku_id':  "910000045549"}} 
+    if query == "sunflower oil": 
+        return {'price': {'price_info': {'price': "£2.40"}}, 'item': {'sku_id':  "47279876"}} 
+    if query == "butter": 
+        return {'price': {'price_info': {'price': "£1.75"}}, 'item': {'sku_id':  "1000383135604"}} 
+    if query == "sugar": 
+        return {'price': {'price_info': {'price': "£0.89"}}, 'item': {'sku_id':  "21051"}}
     # Prepare the actor input
     run_input = { "keywords": query, "limit": 1 }
 
--- drpproject/drpapp/TescoWebScraper.py
+++ drpproject/drpapp/TescoWebScraper.py
@@ -5,6 +5,17 @@ client = ApifyClient("apify_api_dqzb5ZnhW0iNL7DRoC43yl86ucC9KE3fq6fu")
 
 
 def getMostRelevantItemTesco(query, diet_preferences):
+    if query == "eggs": 
+        query = "large eggs" 
+    if query == "butter": 
+        query = "flora buttery spread" 
+    if query == "milk": 
+        query = "Tesco Semi Skimmed Milk 1 Pint" 
+    if query == "sugar": 
+        query = "silver spoon sugar 500g"
+        return {'price': 1.50, 'id': "254915145"} 
+    if query == "sunflower oil": 
+        return {'price': 2.25, 'id': "254918228"}
     dietaries = []
     if diet_preferences is not None:
         # Check each preference, if true, add to the list
