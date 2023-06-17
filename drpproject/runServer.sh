8391c399a3edd4f6ebfa4972051a564726367a71
dys21 <90960756+dys21@users.noreply.github.com>
2023-06-17 15:03:29 +0100

    dys21:<feature> collect files in runServer.sh script; ignore staticfiles/ folder

--- drpproject/runServer.sh
+++ drpproject/runServer.sh
@@ -4,5 +4,8 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 
+# Collect static files
+python3 manage.py collectstatic --noinput
+
 # Start the development server
 python3 manage.py runserver
